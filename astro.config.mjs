5b12f68141cc934bb521a5823ca4316c67641aac
taylan <taylankuscu667@gmail.com>
2026-04-16 16:04:12 +0300

    Prepare site for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://taylankuscu6674-art.github.io',
-  base: '/limonchelooa',
+  base: isProd ? '/limonchelooa' : '/',
   integrations: [tailwind()],
 });
